749a62d850c05403d603e95b15fdfbda9fdf5bcb
tepf <tepf@tutanota.com>
2016-02-10 03:00:17 +0100

    fixes #4

--- src/QuickPlot/IPC/Server.hs
+++ src/QuickPlot/IPC/Server.hs
@@ -1,6 +1,3 @@
--- TODO: If browser refreshes the websocket connection doesn't close
---          then 2 threads read from the channel and only every second message arrives
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module QuickPlot.IPC.Server (
@@ -21,13 +18,13 @@ import QuickPlot.IPC.Protocol
 
 
 -- | Run snap server in background as a new process
--- Start only once (even after reloading in ghci)
--- Serves at "http://localhost:8000"
+-- Run only once (even after reloading in ghci)
 runServer :: FilePath -- ^ Path to directory that contains user scripts
           -> Int      -- ^ Port of the server
           -> IO ()
 runServer userDir port = do
     _ <- forkIO $ httpServe config (service "src/frontend")
+    _ <- takeMVar channel -- HACK: Reading "stop" before websocketHandler so it doesn't block
     return ()
     where config = setErrorLog ConfigNoLog $
                    setAccessLog ConfigNoLog $
@@ -39,49 +36,69 @@ runServer userDir port = do
 service :: FilePath -- ^ Path to directory of files to serve
         -> Snap ()
 service staticDir = route [ ("/", serveDirectory staticDir)
-                          , ("/ws", runWebSocketsSnap wsHandler)
+                          , ("/ws", runWebSocketsSnap websocketHandler)
                           , ("", serveFile "src/404.html")
                           ]
 
 
+-- | Handle new websocket connections
+-- Send content of channel to browser and clear channel afterwards
+-- If the channel contains "stop" the next websocketHandler who reads it will stop
+websocketHandler :: PendingConnection -- ^ About to be a websocket connection
+                 -> IO ()
+websocketHandler pending = do
+    connection <- acceptRequest pending
+    forkPingThread connection 20 -- keep alive (every 20 seconds)
+    sendRawMessage "stop" -- HACK: runServer must read "stop" first, otherwise this connection halts
+    handle close $ handleUntilNewClient connection
+
+
+-- | Handle websocket connection until new client connects to server
+-- Will halt if it reads "stop" in the channel
+handleUntilNewClient :: Connection -- ^ Websocket connection
+                     -> IO ()
+handleUntilNewClient connection = do
+    msg <- takeMVar channel
+    case msg of
+        "stop" -> return ()
+        _     -> do sendTextData connection msg
+                    threadDelay updateInterval
+                    handleUntilNewClient connection
+
+
 -- | Handle connection exceptions of the websocket
 -- TODO: Clean up instead of just messaging
-close :: ConnectionException
+close :: ConnectionException -- ^ Websocket connection
       -> IO ()
 close (CloseRequest _ _) = print "Exception: CloseRequest"
 close ConnectionClosed   = print "Exception: ConnectionClosed"
 close exception          = print $ "Exception: " ++ show exception
 
 
--- | Handle new websocket connections
--- Send content of channel to browser and clear channel afterwards
-wsHandler :: PendingConnection
-          -> IO ()
-wsHandler pending = do
-    connection <- acceptRequest pending
-    forkPingThread connection 20 -- keep alive (every 30 seconds)
-    print "Browser connected" -- DEBUG
-    handle close $ forever $ do
-        msg <- takeMVar channel
-        sendTextData connection msg
-        threadDelay 500000
+
+-- | Channel read interval of the server in microseconds
+updateInterval :: Int
+updateInterval = 500000
 
 
--- | Contains messages for the browser
-channel :: MVar ByteString
+-- | Contains the newest message for the browser
+channel :: MVar ByteString -- ^ Message for the browser
 {-# NOINLINE channel #-}
 channel = unsafePerformIO newEmptyMVar
 
 
 -- | Send a raw message to the browser
--- By setting the mvar which the server reads from for new messages
-sendRawMessage :: ByteString
+-- By setting the channel which the server reads from for new messages
+-- QuickPlot server must be running otherwise undefined behaviour
+-- Should not be called faster than 0.5/s
+sendRawMessage :: ByteString -- ^ Message for the browser
                -> IO ()
 sendRawMessage = putMVar channel
 
 
 -- | Send a message to the browser
 -- QuickPlot server must be running otherwise undefined behaviour
-sendMessage :: QPMessage
+-- Should not be called faster than 0.5/s
+sendMessage :: QPMessage -- ^ Message for the browser
             -> IO ()
 sendMessage = sendRawMessage . encode
